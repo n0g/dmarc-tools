d8061204ee0aa7b8dc7afb00034bf32ccf59ef6b
Matthias Fassl <mf@n0g.at>
2014-02-06 16:11:39 +0100

    Read DMARC Report (unzipped) and print human readable version

---
+++ dmarc2human.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import xml.etree.ElementTree as et
+import sys
+import time
+import socket
+
+# parse input
+root = et.parse(sys.stdin)
+result = ''
+
+# print metadata
+org = root.find('./report_metadata/org_name').text
+org_mail = root.find('./report_metadata/email').text
+begin = time.ctime(float(root.find('./report_metadata/date_range/begin').text))
+end = time.ctime(float(root.find('./report_metadata/date_range/end').text))
+
+print "Organization: " + org + " (" + org_mail + ")"
+print begin + " - " + end
+
+# print policy
+# TODO: not really that important - i know my own policy
+
+# print records
+for row in root.findall('./record/row'):
+	num = row.find('./count').text
+	src = row.find('./source_ip').text
+	src_domain = socket.gethostbyaddr(src)[0]
+	dkim = row.find('./policy_evaluated/dkim').text
+	spf = row.find('./policy_evaluated/spf').text
+	print
+	print num + " Mails from " + src_domain + " (" + src + ")"
+	print "DKIM: " + dkim
+	print "SPF: " + spf
